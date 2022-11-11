91e5b00385a3263476c064cc4928c6f6250327dd
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2022-11-11 18:21:20 +0100

    feat(front): * Create _document.js and layout.tsx for new document for Next13

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html id="HTML" className="dark">
+        <Html id="HTML" className="dark" lang="fr">
             <Head />
             <body>
             <Main />
