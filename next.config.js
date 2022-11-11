fc94e5a7ef8968bfcdd5d0bf4c330f92737c9f23
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2022-11-11 18:14:44 +0100

    feat(config): *Initial commit with tsconfig.json, next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: false,
+  },
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  }
 }
 
 module.exports = nextConfig
