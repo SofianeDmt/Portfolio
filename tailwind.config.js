9bf646bb384f3b905964f20d7ac0d58a212117da
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2022-11-11 18:16:56 +0100

    feat(config): * add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin"),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
