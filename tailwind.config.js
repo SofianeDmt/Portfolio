51d9f757c8144214ae83381fd33fa89f196b8845
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2022-11-11 18:47:08 +0100

    fix(config): * Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,15 @@
  * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
  */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./node_modules/flowbite-react/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin"),
-    require('@tailwindcss/line-clamp'),
-  ],
-}
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/line-clamp")],
+};
