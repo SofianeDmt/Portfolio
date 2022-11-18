868f15ab8ba2a992a6df5fadb522e0cdc7b2cf7a
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2022-11-18 10:25:42 +0100

    fix(config): * Fix package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,8 @@ module.exports = {
   darkMode: "class",
   content: [
     "./node_modules/flowbite-react/**/*.js",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {},
