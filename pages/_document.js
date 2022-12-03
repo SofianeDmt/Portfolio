b1001f58d3a5e13d1a05dc7889e5b67eaf9b33c5
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2022-12-03 15:58:54 +0100

    feat(front): * add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html id="HTML" className="" lang="fr">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/public/images/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
