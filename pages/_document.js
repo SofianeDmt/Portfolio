0c71e1ffea864b5399fbe56ad16b51ae7de4384c
Sofiane Demette <Sofiane.dmt@hotmail.fr>
2022-12-08 14:15:44 +0100

    feat(front): * add SEO balise

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html id="HTML" className="" lang="fr">
+    <Html id="HTML" className="" lang="fr-FR">
       <Head>
         <link rel="icon" href="/images/favicon.ico" />
       </Head>
